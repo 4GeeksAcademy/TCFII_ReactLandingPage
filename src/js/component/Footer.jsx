72f8e4cbbe37c4c61e118d51d9a10f6419f49d1a
TCFII <168501077+TCFII@users.noreply.github.com>
2024-07-02 00:27:53 +0000

    FormatOK_1

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Footer() {
+        return (
+            <div className ='w-full bg-gray-500 rounded flex flex-col items-center justify center h-10'>Footer</div>
+        );
+    }
---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Jumbotron() {
+        return (
+            <div className ='w-full bg-blue-500 rounded flex flex-col items-center justify center h-10'>Jumbotron</div>
+        );
+    }
