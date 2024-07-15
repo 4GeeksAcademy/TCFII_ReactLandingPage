7a6e2e63261b26655e2dc65d3deb6f5ca4deb434
TCFII <168501077+TCFII@users.noreply.github.com>
2024-07-15 22:10:59 +0000

    more work 071524

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -5,11 +5,19 @@ export function Card() {
             /*<div className ='w-full bg-red-100 rounded flex flex-col items-center justify center h-10'>*/
                 <div className="max-w-xs mx-auto bg-gray-500 shadow-lg rounded-lg overflow-hidden">
                     <div className="bg-white px-4 py-2">
-                        <h2 className="text-gray-800 text-xl font-semibold">Card Title</h2>
+                        <h2 className="text-gray-800 text-xl font-semibold font-bold flex justify-center items-center">Card Topper</h2>
                         <p className="text-gray-600 mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
-                    <div className="flex justify-end px-4 pb-4">
-                        <a href="#" className="text-sm bg-blue-500 hover:bg-blue-600 text-white py-1 px-3 rounded">Read More</a>
+                    <div>
+                        <div>
+                            <h1 className="font-bold flex justify-center items-center">Card Title</h1>
+                        </div>
+                        <div>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla feugiat accumsan felis. Etiam eget rhoncus dui. Donec mattis dignissim consectetur. Lorem ipsum dolor sit amet.
+                        </div>
+                        <div>
+                        <a href="#" className="text-sm  bg-blue-500 flex justify-center hover:bg-blue-600 text-white py-1 px-3 rounded">Read More</a>
+                        </div>
                     </div>
                 </div>
             /*</div>*/
