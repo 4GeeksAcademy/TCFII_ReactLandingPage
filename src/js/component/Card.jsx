f84020edbb78c331407a7635dc4b00f14afa8d9b
TCFII <168501077+TCFII@users.noreply.github.com>
2024-07-19 23:28:08 +0000

    Updated 071924

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 export function Card() {
         return (
-            /*<div className ='w-full bg-red-100 rounded flex flex-col items-center justify center h-10'>*/
-                <div className="max-w-xs mx-auto bg-gray-500 shadow-lg rounded-lg overflow-hidden">
-                    <div className="bg-white px-4 py-2">
-                        <h2 className="text-gray-800 text-xl font-semibold font-bold flex justify-center items-center">Card Topper</h2>
-                        <p className="text-gray-600 mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            /*<div className ='bg-red-100 rounded flex flex-col items-center justify center h-10'>*/
+                <div className="w-2/3 mx-auto bg-gray-500 shadow-lg rounded-lg overflow-hidden">
+                    <div>
+                        <img class="h-auto max-w-full rounded-lg" src="https://placehold.co/500x325" alt="image description"/>
                     </div>
                     <div>
                         <div>
@@ -15,8 +14,8 @@ export function Card() {
                         <div>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla feugiat accumsan felis. Etiam eget rhoncus dui. Donec mattis dignissim consectetur. Lorem ipsum dolor sit amet.
                         </div>
-                        <div>
-                        <a href="#" className="text-sm  bg-blue-500 flex justify-center hover:bg-blue-600 text-white py-1 px-3 rounded">Read More</a>
+                        <div className="flex justify-center items-center">
+                        <button><a href="#" className="text-sm  bg-blue-500 flex justify-center hover:bg-blue-600 text-white py-1 px-3 rounded">Read More</a></button>
                         </div>
                     </div>
                 </div>
--- src/js/component/CardRow.jsx
+++ src/js/component/CardRow.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export function CardRow({children}) {
     return (
-        <div className ="grid grid-cols-1 lg:grid-cols-4 gap-4">{children}</div>
+        <div className ="w-2/3 justify-center items-center grid grid-cols-1 lg:grid-cols-4 gap-4">{children}</div>
     );
 }
